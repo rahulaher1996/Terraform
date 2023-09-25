307863e371d84ce0d37a8534f48dc3076648f719
rahulaher1996 <106946116+rahulaher1996@users.noreply.github.com>
2023-09-26 01:38:27 +0530

    IAM_combine.tf

---
+++ AWS/IAM_combine.tf
@@ -0,0 +1,57 @@
+resource "aws_iam_user" "test_user" {
+  name = "test-user"
+}
+
+resource "aws_iam_group_membership" "test_user_group_membership" {
+  name       = "test group"
+  users      = [aws_iam_user.test_user.name]
+  group      = aws_iam_group.test_group.name
+}
+
+resource "aws_iam_group" "test_group" {
+  name = "test-group"
+}
+
+resource "aws_iam_policy" "ec2_full_access" {
+  name        = "ec2-full-access-policy"
+  description = "Policy for EC2 full access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = "ec2:*",
+        Effect   = "Allow",
+        Resource = "*",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy" "s3_full_access" {
+  name        = "s3-full-access-policy"
+  description = "Policy for S3 full access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = "s3:*",
+        Effect   = "Allow",
+        Resource = "*",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy_attachment" "ec2_full_access_attachment" {
+  name       = "ec2-full-access-attachment"
+  policy_arn = aws_iam_policy.ec2_full_access.arn
+  users      = [aws_iam_user.test_user.name]
+}
+
+resource "aws_iam_policy_attachment" "s3_full_access_attachment" {
+  name       = "s3-full-access-attachment"
+  policy_arn = aws_iam_policy.s3_full_access.arn
+  users      = [aws_iam_user.test_user.name]
+}
