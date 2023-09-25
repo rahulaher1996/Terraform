6947b15703931bef248c6a96043f931b1a38a66c
rahulaher1996 <106946116+rahulaher1996@users.noreply.github.com>
2023-09-26 01:08:29 +0530

    IAM_user.tf

---
+++ AWS/IAM_user.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_user" "test_user" {
+  name = "test-user"
+}
+
+resource "aws_iam_access_key" "test_user_access_key" {
+  user = aws_iam_user.test_user.name
+}
+
+resource "aws_iam_user_login_profile" "test_user_login_profile" {
+  user                    = aws_iam_user.test_user.name
+  password_reset_required = false
+  pgp_key                 = ""
+  password_length         = 10
+  password               = "Password@12345"
+}
