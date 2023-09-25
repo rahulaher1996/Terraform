597cfcb1b9458ffc607f525854acb6eae900d7b4
rahulaher1996 <106946116+rahulaher1996@users.noreply.github.com>
2023-09-26 01:10:08 +0530

    IAM_group.tf

---
+++ AWS/IAM_group.tf
@@ -0,0 +1,13 @@
+resource "aws_iam_group" "test_group" {
+  name = "test group"
+}
+
+resource "aws_iam_user" "test_user" {
+  name = "test-user"
+}
+
+resource "aws_iam_group_membership" "test_user_group_membership" {
+  name       = "test group"
+  users      = [aws_iam_user.test_user.name]
+  group      = aws_iam_group.test_group.name
+}
